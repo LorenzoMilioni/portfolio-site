b365f02dfe168af372ddb5b88c202d4c3e8d57f0
LorenzoMilioni <125701490+LorenzoMilioni@users.noreply.github.com>
2023-05-10 05:49:21 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,37 +1,56 @@
-const words = ["Nulla", "facilisi", "Cras sed odio sed leo laoreet molestie id non purus.."];
-let i = 0;
-let counter;
-function typeNow() {
-   let word = words[i].split("");
-   var loopTyping = function() {
-      if (word.length > 0) {
-         document.getElementById('text').innerHTML += word.shift();
-      } else {
-         deleteNow();
-         return false;
-      };
-      counter = setTimeout(loopTyping, 200);
-   };
-   loopTyping();
-};
-function deleteNow() {
-   let word = words[i].split("");
-   var loopDeleting = function() {
-      if (word.length > 0) {
-         word.pop();
-         document.getElementById('text').innerHTML = word.join("");
-      } else {
-         if (words.length > (i + 1)) {
-            i++;
-         }
-         else {
-            i = 0;
-         };
-         typeNow();
-         return false;
-      };
-      counter = setTimeout(loopDeleting, 200);
-   };
-   loopDeleting();
-};
-typeNow();
+var TxtRotate = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+  };
+  
+  TxtRotate.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+  
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+  
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  
+    var that = this;
+    var delta = 300 - Math.random() * 100;
+  
+    if (this.isDeleting) { delta /= 2; }
+  
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
+    }
+  
+    setTimeout(function() {
+      that.tick();
+    }, delta);
+  };
+  
+  window.onload = function() {
+    var elements = document.getElementsByClassName('txt-rotate');
+    for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-rotate');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtRotate(elements[i], JSON.parse(toRotate), period);
+      }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+    document.body.appendChild(css);
+  };
